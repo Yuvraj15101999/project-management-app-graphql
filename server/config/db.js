a1a6376deb055a31e126b3db00a892687e4d9ded
Yuvraj Umesh Pawar <yuvrajpawar2215@gmail.com>
2023-02-15 21:03:56 +0530

    issue removed

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({path:'../.env'});
 
 const mongooseConnectionString =
-  "mongodb+srv://yuvraj-umesh-pawar-15101999:hardwork100@cluster0.n9oqbpj.mongodb.net/mgmt_db?retryWrites=true&w=majority";
+  "";
 
 const connectDB = async () => {
   const conn = await mongoose.connect(mongooseConnectionString);
